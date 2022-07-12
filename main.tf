0f76b55bdedf2034bdb8252fa0f0864e3227a784
David Hoatson <davidhoatson@gmail.com>
2022-07-12 11:18:29 +0000

    Add labels

--- main.tf
+++ main.tf
@@ -46,6 +46,11 @@ resource "tls_private_key" "ssh-key" {
 resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
+  labels       {
+    department = "devops"
+    billable   = "true"
+  }
+
   machine_type = var.machine_type
 
   boot_disk {
